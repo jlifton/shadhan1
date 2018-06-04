448e6794fa455b844134aab8e5e5ab2af42bbd23
Jonathan Lifton <jonlifton@gmail.com>
2018-06-04 22:17:23 +0300

    Operators service class

---
+++ public/src/app/shadhan/operators/operators-table/operators.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs/Rx";
+import {OperatorDTO} from "./operator.data";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json',
+  'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YWVmNGFlYzdlNDliODJiNzRhNDIxMGYiLCJpYXQiOjE1MjU2MzE3MjR9.tZR5o0VIf0fQ20pFPWwnzLnw_T516xh4B6ukKp1uRgw'})
+
+};
+@Injectable({
+  providedIn: 'root'
+})
+export class OperatorsService {
+
+  constructor(private http:HttpClient) { }
+
+    getOperators (): Observable<OperatorDTO[]> {
+      return this.http.get<OperatorDTO[]>('http://localhost:3000/api/operators', httpOptions);
+    }
+
+}
+
