1ede8fdeb22d2adf94d210324a5dc84763981e63
Jonathan Lifton <jonlifton@gmail.com>
2018-06-10 00:22:59 +0300

    Differentiate new vs edit operator (needed for password handling)

--- routes/operators.js
+++ routes/operators.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
-const {Operator, validate, validateLogin} = require('../models/operator');
+const {Operator, validateNewOperator, validateUpdateOperator, validateLogin} = require('../models/operator');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -53,21 +53,21 @@ router.post('/login', async (req, res) => {
 
 
 router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
+    const { error } = validateNewOperator(req.body);
 if (error) return res.status(400).send(error.details[0].message);
 
 let operator = await Operator.findOne({ email: req.body.email });
 if (operator) return res.status(400).send('Operator already registered.');
 
 operator = new Operator(_.pick(req.body, ['name', 'type', 'username', 'password', 'email',
-                                            'phone', 'street', 'city', 'country']));
+                                            'phone', 'street', 'city', 'country', 'notes']));
 const salt = await bcrypt.genSalt(10);
 operator.password = await bcrypt.hash(operator.password, salt);
 await operator.save();
 
 const token = operator.generateAuthToken();
 res.header('x-auth-token', token).send(_.pick(operator, ['_id',
-    'name', 'type', 'username', 'email', 'phone', 'street', 'city', 'country']));
+    'name', 'type', 'username', 'email', 'phone', 'street', 'city', 'country', 'notes']));
 });
 
 router.delete('/:id', [auth, validateObjectId], async (req, res) => {
@@ -87,19 +87,20 @@ router.get('/:id', [auth, validateObjectId], async (req, res) => {
 });
 
 router.put('/:id', [auth, validateObjectId], async (req, res) => {
-    const { error } = validate(req.body);
+    const { error } = validateUpdateOperator(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     const operator = await Operator.findByIdAndUpdate(req.params.id,
                             { name: req.body.name ,
                              type: req.body.type ,
                              email: req.body.email ,
-                             password: req.body.password ,
+                             //password: req.body.password ,
                              phone: req.body.phone ,
                              street: req.body.street ,
                              city: req.body.city ,
                              country: req.body.country ,
-                             username: req.body.username },
+                             username: req.body.username,
+                             notes: req.body.notes},
         function(err, result){
             const x = '';
         });
