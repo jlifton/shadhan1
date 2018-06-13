01d00df4461c040ad6f0f354137f0ef909768404
Jonathan Lifton <jonlifton@gmail.com>
2018-06-13 19:37:14 +0300

    SinglesTableComponent and modules created

---
+++ public/src/app/shadhan/singles/singles-table/singles-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fury-singles-table',
+  templateUrl: './singles-table.component.html',
+  styleUrls: ['./singles-table.component.scss']
+})
+export class SinglesTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ public/src/app/shadhan/singles/singles-table/singles-table.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatDialogModule, MatSortModule} from "@angular/material";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";;
+import {SinglesTableRoutingModule} from "./singles-table-routing.module";
+import {SinglesTableComponent} from "./singles-table.component";
+import {PageModule} from "../../../core/common/page/page.module";
+import {MaterialModule} from "../../../core/common/material-components.module";
+import {BreadcrumbsModule} from "../../../core/breadcrumbs/breadcrumbs.module";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    SinglesTableRoutingModule,
+    MaterialModule,
+    MatDialogModule,
+    MatSortModule,
+    ReactiveFormsModule,
+    PageModule,
+    BreadcrumbsModule
+  ],
+  declarations: [SinglesTableComponent],
+  providers: [],//[SinglesService],
+  exports: [SinglesTableComponent]//,
+  //entryComponents: [OperatorDialogComponent]
+})
+export class SinglesTableModule { }
+
