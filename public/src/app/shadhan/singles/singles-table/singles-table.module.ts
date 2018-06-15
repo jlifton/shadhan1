51c425fb3f04263657968b56556be8e4a7a0d69f
Jonathan Lifton <jonlifton@gmail.com>
2018-06-15 09:17:56 +0300

    Cosmetic

--- public/src/app/shadhan/singles/singles-table/singles-table.module.ts
+++ public/src/app/shadhan/singles/singles-table/singles-table.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatDialogModule, MatSortModule} from "@angular/material";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";;
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SinglesTableRoutingModule} from "./singles-table-routing.module";
 import {SinglesTableComponent} from "./singles-table.component";
 import {PageModule} from "../../../core/common/page/page.module";
