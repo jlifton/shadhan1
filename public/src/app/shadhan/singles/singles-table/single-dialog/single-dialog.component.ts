7c33905438b5075f7802c0f36d0a8df5fc3b8fb6
Jonathan Lifton <jonlifton@gmail.com>
2018-06-25 21:46:23 +0300

    dateUpdate added; trim() added

--- public/src/app/shadhan/singles/singles-table/single-dialog/single-dialog.component.ts
+++ public/src/app/shadhan/singles/singles-table/single-dialog/single-dialog.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectorRef, Component, Inject, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {MAT_DIALOG_DATA, MatDialogRef, MatSnackBar} from "@angular/material";
 import {SingleDTO} from "../single.data";
 
@@ -18,6 +18,7 @@ export class SingleDialogComponent implements OnInit {
   title: string;
   isNew = true;
   commitLabel: string;
+  dateUpdate: string;
 
   constructor(private fb: FormBuilder,
               private cd: ChangeDetectorRef,
@@ -29,15 +30,18 @@ export class SingleDialogComponent implements OnInit {
       this.isNew = false;
   }
   ngOnInit() {
+    this.dateUpdate = '';
     this.title = this.isNew ? 'New Single' : 'Modify Single';
     this.commitLabel =  this.isNew ? 'CREATE SINGLE' : 'MODIFY SINGLE';
+    if (!this.isNew)
+      this.dateUpdate = this.toDateTimeString();
     this.identityFormGroup = this.fb.group({
       lastName: [this.singleDTO.identity.lastName, [Validators.required,  Validators.maxLength(64)]],
       firstName: [this.singleDTO.identity.firstName, [Validators.required,  Validators.maxLength(50)]],
       sex: [this.singleDTO.identity.sex, [Validators.required]],
       age: [this.singleDTO.identity.age, [Validators.required, Validators.min(16), Validators.max(120)]],
-      maritalStatus: [this.singleDTO.identity.maritalStatus, [Validators.required]]
-
+      maritalStatus: [this.singleDTO.identity.maritalStatus, [Validators.required]],
+      updated:  [this.singleDTO.updated, []]
     });
 
     this.backgroundFormGroup = this.fb.group({
@@ -80,8 +84,8 @@ export class SingleDialogComponent implements OnInit {
   }
 
  submit() {
-   this.singleDTO.identity.lastName = this.identityFormGroup.value.lastName;
-   this.singleDTO.identity.firstName = this.identityFormGroup.value.firstName;
+   this.singleDTO.identity.lastName = this.identityFormGroup.value.lastName.trim();
+   this.singleDTO.identity.firstName = this.identityFormGroup.value.firstName.trim();
    this.singleDTO.identity.sex = this.identityFormGroup.value.sex;
    this.singleDTO.identity.age = this.identityFormGroup.value.age;
    this.singleDTO.identity.maritalStatus = this.identityFormGroup.value.maritalStatus;
@@ -89,32 +93,40 @@ export class SingleDialogComponent implements OnInit {
    this.singleDTO.religioEthnic.hashkafa = this.backgroundFormGroup.value.hashkafa;
    this.singleDTO.religioEthnic.ethnicity = this.backgroundFormGroup.value.ethnicity;
    this.singleDTO.religioEthnic.primaryActivity = this.backgroundFormGroup.value.primaryActivity;
-   this.singleDTO.occupation = this.backgroundFormGroup.value.occupation;
-   this.singleDTO.pastEducation = this.backgroundFormGroup.value.pastEducation;
-   this.singleDTO.religioEthnic.ethnicityAdditional = this.backgroundFormGroup.value.ethnicityAdditional;
+   this.singleDTO.occupation = this.backgroundFormGroup.value.occupation.trim();
+   this.singleDTO.pastEducation = this.backgroundFormGroup.value.pastEducation.trim();
+   this.singleDTO.religioEthnic.ethnicityAdditional = this.backgroundFormGroup.value.ethnicityAdditional.trim();
    this.singleDTO.religioEthnic.cohen = this.backgroundFormGroup.value.cohen;
    this.singleDTO.religioEthnic.convert = this.backgroundFormGroup.value.convert;
 
-   this.singleDTO.residence.city = this.residenceContactFormGroup.value.city;
-   this.singleDTO.residence.country = this.residenceContactFormGroup.value.country;
-   this.singleDTO.contact.name = this.residenceContactFormGroup.value.name;
-   this.singleDTO.contact.relationship = this.residenceContactFormGroup.value.relationship;
-   this.singleDTO.contact.primaryPhone = this.residenceContactFormGroup.value.primaryPhone;
-   this.singleDTO.contact.secondaryPhone = this.residenceContactFormGroup.value.secondaryPhone;
-   this.singleDTO.contact.email = this.residenceContactFormGroup.value.email;
+   this.singleDTO.residence.city = this.residenceContactFormGroup.value.city.trim();
+   this.singleDTO.residence.country = this.residenceContactFormGroup.value.country.trim();
+   this.singleDTO.contact.name = this.residenceContactFormGroup.value.name.trim();
+   this.singleDTO.contact.relationship = this.residenceContactFormGroup.value.relationship.trim();
+   this.singleDTO.contact.primaryPhone = this.residenceContactFormGroup.value.primaryPhone.trim();
+   this.singleDTO.contact.secondaryPhone = this.residenceContactFormGroup.value.secondaryPhone.trim();
+   this.singleDTO.contact.email = this.residenceContactFormGroup.value.email.trim();
 
    this.singleDTO.physical.height = this.physicalFormGroup.value.height;
-   this.singleDTO.physical.build = this.physicalFormGroup.value.build;
-   this.singleDTO.physical.description = this.physicalFormGroup.value.description;
-   this.singleDTO.personalityRequirements = this.physicalFormGroup.value.personalityRequirements;
-   this.singleDTO.specialNeeds = this.physicalFormGroup.value.specialNeeds;
+   this.singleDTO.physical.build = this.physicalFormGroup.value.build.trim();
+   this.singleDTO.physical.description = this.physicalFormGroup.value.description.trim();
+   this.singleDTO.personalityRequirements = this.physicalFormGroup.value.personalityRequirements.trim();
+   this.singleDTO.specialNeeds = this.physicalFormGroup.value.specialNeeds.trim();
    this.singleDTO.physical.smoker = this.physicalFormGroup.value.smoker;
 
-   this.singleDTO.source.name = this.sourceFormGroup.value.name;
-   this.singleDTO.source.email = this.sourceFormGroup.value.email;
-   this.singleDTO.source.phone = this.sourceFormGroup.value.phone;
-   this.singleDTO.comments = this.sourceFormGroup.value.comments;
+   this.singleDTO.source.name = this.sourceFormGroup.value.name.trim();
+   this.singleDTO.source.email = this.sourceFormGroup.value.email.trim();
+   this.singleDTO.source.phone = this.sourceFormGroup.value.phone.trim();
+   this.singleDTO.comments = this.sourceFormGroup.value.comments.trim();
 
    this.dialogRef.close(this.singleDTO);
+  }
+
+  toDateTimeString() {
+    let d: Date;
+    d = new Date(this.singleDTO.updated);
+    var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" +
+      d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+    return datestring;
   }
 }
