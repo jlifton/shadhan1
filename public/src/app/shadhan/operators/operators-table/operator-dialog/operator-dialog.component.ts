7933958cb1097185a7b45bc5170008a31eeffcf7
Jonathan Lifton <jonlifton@gmail.com>
2018-07-23 21:50:03 +0300

    Added phone number validator

--- public/src/app/shadhan/operators/operators-table/operator-dialog/operator-dialog.component.ts
+++ public/src/app/shadhan/operators/operators-table/operator-dialog/operator-dialog.component.ts
@@ -36,7 +36,7 @@ export class OperatorDialogComponent implements OnInit {
       title: [this.title, []],
       name: [this.operatorDTO.name, [Validators.required, Validators.minLength(5),  Validators.maxLength(50)]],
       type: [this.operatorDTO.type, [Validators.required]],
-      phone: [this.operatorDTO.phone, [Validators.required,  Validators.maxLength(64)]],
+      phone: [this.operatorDTO.phone, [Validators.required,  Validators.pattern('[0-9]+'), Validators.maxLength(64)]],
       email: [this.operatorDTO.email, [Validators.required, Validators.email,  Validators.maxLength(255)]],
       street: [this.operatorDTO.street, [ Validators.maxLength(255)]],
       city: [this.operatorDTO.city, [ Validators.maxLength(255)]],
