080600fdea022d4b93adbd56f233a98dd7c3e980
Jonathan Lifton <jonlifton@gmail.com>
2018-07-28 21:42:07 +0300

    List of removed files after refactoring node + angular to be together

--- public/src/app/core/widgets/bar-chart-widget/bar-chart-widget-options.interface.ts
+++
@@ -1,4 +0,0 @@
-import { ChartWidgetOptions } from '../../common/chart-widget/chart-widget-options.interface';
-
-export class BarChartWidgetOptions extends ChartWidgetOptions {
-}
--- public/src/app/core/widgets/line-chart-widget/line-chart-widget-options.interface.ts
+++
@@ -1,8 +0,0 @@
-import { ChartWidgetOptions } from '../../common/chart-widget/chart-widget-options.interface';
-
-export class LineChartWidgetOptions extends ChartWidgetOptions {
-  gradientFill?: {
-    from: string;
-    to: string;
-  };
-}
