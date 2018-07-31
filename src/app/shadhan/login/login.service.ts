1aaa5e302be53ecee9b7ec57ff4b2b144dc5823d
Jonathan Lifton <jonlifton@gmail.com>
2018-07-31 21:38:07 +0300

    Added isDeployed

--- src/app/shadhan/login/login.service.ts
+++ src/app/shadhan/login/login.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
 import {Observable} from "rxjs/Rx";
 import {LoginResponse} from "./login-response.interface";
-const deployed = true;
+import {AppConstants} from '../../constants';
+
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json'}),
   observe: "response" as 'body' // to display the full response & as 'body' for type cast
@@ -19,7 +20,7 @@ export class LoginService {
 
   login(username, password):Observable<HttpResponse<LoginResponse>> {
     var obj = { username: username, password: password};
-    if (deployed)
+    if (AppConstants.isDeployed)
       return  this.http.post <HttpResponse<LoginResponse>>('/api/operators/login', obj, httpOptions);
     else
       return  this.http.post <HttpResponse<LoginResponse>>('http://localhost:3000/api/operators/login', obj, httpOptions);
