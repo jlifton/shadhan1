cb16e18ccb1b048ef4d6d975f39c68a7797cd504
Jonathan Lifton <jonlifton@gmail.com>
2018-08-18 20:41:32 +0300

    Cosmetics

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,15 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const winston = require('winston');
 
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+  if (!token)
+    return res.status(401).send('Access denied. No token provided.');
 
   try {
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    req.user = decoded; 
+    req.user = decoded;
     next();
   }
   catch (ex) {
