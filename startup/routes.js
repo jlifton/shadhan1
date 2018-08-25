c0e5ad7837b2bb8b0a1a687f8d58bde8483ff24f
Jonathan Lifton <jonlifton@gmail.com>
2018-08-25 22:11:08 +0300

    Removed vidly references

--- startup/routes.js
+++ startup/routes.js
@@ -1,33 +1,15 @@
 const express = require('express');
-//const genres = require('../routes/genres');
-//const customers = require('../routes/customers');
-//const movies = require('../routes/movies');
-//const rentals = require('../routes/rentals');
-//const users = require('../routes/users');
-//const auth = require('../routes/auth');
-//const returns = require('../routes/returns');
 const operators = require('../routes/operators');
 const singles = require('../routes/singles');
 const archives = require('../routes/archives');
-//const logitems = require('../routes/logitems');
-//const systems= require('../routes/systems');
 const error = require('../middleware/error');
 const cors = require('cors');
 
 module.exports = function(app) {
   app.use(express.json());
   app.use(cors());
-  //app.use('/api/genres', genres);
-  //app.use('/api/customers', customers);
-  //app.use('/api/movies', movies);
-  //app.use('/api/rentals', rentals);
-  //app.use('/api/users', users);
-  //app.use('/api/auth', auth);
-  //app.use('/api/returns', returns);
   app.use('/api/operators', operators);
   app.use('/api/singles', singles);
   app.use('/api/archives', archives);
-  //app.use('/api/logitems', logitems);
-  //app.use('/api/systems', systems);
   app.use(error);
 };
